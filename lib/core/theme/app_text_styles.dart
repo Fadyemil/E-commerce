e25b10fc32559caacb24bae3f4b40850b59b0681
fady emil <fady46t45f@gmail.com>
2024-11-04 19:14:36 +0200

    Add app text styles for consistent typography across the application
    This commit introduces a new file, `app_text_styles.dart`, which defines a comprehensive set of text styles for use throughout the application. The styles are categorized into header and body styles, each with bold and regular variants. This ensures consistent typography and improves readability across different sections of the app. The styles are designed to be responsive using the `flutter_screenutil` package, ensuring that text scales appropriately on various screen sizes.

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyles {
+  //~ Header Styles
+
+  static final TextStyle heading48Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 48.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading40Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 40.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading33Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 33.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading28Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 28.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading23Bold = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 23.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading48Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 48.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading40Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 40.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading33Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 33.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading28Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 28.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle heading23Regular = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 23.sp,
+    height: 1.4.h,
+  );
+
+  //~ Body Styles
+
+  static final TextStyle bodyLargeBold19 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 19.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle bodyBasaBold16 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 16.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle bodySmallBold13 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 13.sp,
+    height: 1.7.h,
+  );
+
+  static final TextStyle bodyXSmallBold11 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w700,
+    fontSize: 11.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle bodyLargeRegular19 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 19.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle bodyBasaRegular16 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 16.sp,
+    height: 1.4.h,
+  );
+
+  static final TextStyle bodySmallRegular13 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 13.sp,
+    height: 1.6.h,
+  );
+
+  static final TextStyle bodyXSmallRegular11 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w400,
+    fontSize: 11.sp,
+    height: 1.4.h,
+  );
+}
