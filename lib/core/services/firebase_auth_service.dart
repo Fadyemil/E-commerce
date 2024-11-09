9f9bbdfc61088c57ba1c5fceddb5d26117eb3d70
fady emil <fady46t45f@gmail.com>
2024-11-09 19:51:26 +0200

    Add sign-in functionality with email and password to FirebaseAuthService
    This commit introduces a new method `signInWithEmailAndPassword` in the `FirebaseAuthService` class. This method allows users to sign in using their email and password by interacting with Firebase Authentication. It handles various FirebaseAuthException cases, such as user not found, wrong password, network request failure, and invalid credentials, by throwing custom exceptions with appropriate error messages. This enhancement improves the user experience by providing clear and localized error messages during the sign-in process.

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -4,6 +4,7 @@ import 'package:e_commerce/core/errors/exceptions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthService {
+  //~ Create User with Email and Password
   Future<User> createUserWithEmailAndPassword(
       {required String email, required String password}) async {
     try {
@@ -29,6 +30,40 @@ class FirebaseAuthService {
       log('Exception in createUserWithEmailAndPassword $e');
       throw CustomException(
           message: 'لقد حدث خطأ ما. الرجاء المحاولة مرة أخرى.');
+    }
+  }
+
+  //~ Sign In with Email and Password
+  Future<User> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      log('Exception in FirebaseAuthException signInWithEmailAndPassword $e');
+      if (e.code == 'user-not-found') {
+        log('11111111111111111111111111111111111111');
+        throw CustomException(
+            message: 'لا يوجد مستخدم بهذا البريد الإلكتروني.');
+      } else if (e.code == 'wrong-password') {
+        throw CustomException(message: 'كلمة المرور غير صحيحة.');
+      } else if (e.code == 'network-request-failed') {
+        throw CustomException(message: 'تاكد من الاتصال بالانترنت');
+      } else if (e.code == 'invalid-credential') {
+        throw CustomException(
+            message:
+                'بيانات الاعتماد غير صالحة. يرجى التحقق من البريد الإلكتروني وكلمة المرور.');
+      } else {
+        throw CustomException(
+            message: ' *********لقد حدث خطأ ما. الرجاء المحاولة مرة أخرى.');
+      }
+    } catch (e) {
+      log('Exception in signInWithEmailAndPassword $e');
+      throw CustomException(
+          message: ' لقد حدث خطأ ما. الرجاء المحاولة مرة أخرى.');
     }
   }
 }
