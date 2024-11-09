bee121bacd748a39979e421bddcc33a06839d861
fady emil <fady46t45f@gmail.com>
2024-11-09 18:17:34 +0200

    Add AuthRepo interface for user authentication
    This commit introduces the `AuthRepo` interface within the `auth` feature's domain layer. The interface defines a method `createUserwithEmailandPassword` that returns a `Future` of `Either<Faliure, UserEntity>`. This method is responsible for handling user creation with email and password, encapsulating the potential failure or successful creation of a `UserEntity`. The addition of this interface sets the foundation for implementing authentication logic in the application.

---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/errors/faliures.dart';
+import 'package:e_commerce/features/auth/domain/entites/user_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Faliure, UserEntity>> createUserwithEmailandPassword(
+    String email,
+    String password,
+    String name,
+  );
+}
