a1bf69d1059f456dbb9fb639967c56bd07af3578
fady emil <fady46t45f@gmail.com>
2024-11-11 16:18:33 +0200

    Add password field and copyWith method to UserModel and UserEntity
    - Extended the `UserModel` class to include a `password` field, which is required during initialization.
    - Updated the `fromFirebaseUser` factory constructor to optionally accept a `password` parameter.
    - Implemented a `copyWith` method in `UserModel` to allow creating a new instance with updated fields.
    - Added a `password` field to the `UserEntity` class and included it in the `toMap` method for serialization.

--- lib/features/auth/data/model/user_model.dart
+++ lib/features/auth/data/model/user_model.dart
@@ -2,13 +2,29 @@ import 'package:e_commerce/features/auth/domain/entites/user_entity.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel extends UserEntity {
-  UserModel({required super.email, required super.name, required super.uId});
+  UserModel(
+      {required super.email,
+      required super.name,
+      required super.uId,
+      required super.password});
 
-  factory UserModel.fromFirebaseUser(User user) {
+  factory UserModel.fromFirebaseUser(User user, {String? password}) {
     return UserModel(
       email: user.email ?? '',
       name: user.displayName ?? '',
       uId: user.uid,
+      password: password ?? "",
+    );
+  }
+
+  // Define the copyWith method
+  UserModel copyWith(
+      {String? email, String? name, String? uId, String? password}) {
+    return UserModel(
+      email: email ?? this.email,
+      name: name ?? this.name,
+      uId: uId ?? this.uId,
+      password: password ?? this.password,
     );
   }
 }
--- lib/features/auth/domain/entites/user_entity.dart
+++ lib/features/auth/domain/entites/user_entity.dart
@@ -2,10 +2,21 @@ class UserEntity {
   final String name;
   final String email;
   final String uId;
+  final String password;
 
   UserEntity({
     required this.name,
     required this.email,
     required this.uId,
+    required this.password,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'uId': uId,
+      'password': password,
+    };
+  }
 }
