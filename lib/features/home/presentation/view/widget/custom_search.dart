e6e448c137b5bf2ad31b25820c4feb099c64bfc9
fady emil <fady46t45f@gmail.com>
2024-11-13 16:02:47 +0200

    Add SearchTextField and CustomSearch Widgets
    Created a new `SearchTextField` widget in `lib/core/widget/search_text_field.dart` to encapsulate reusable search input field functionality. The widget includes customizable properties such as `contentPadding`, `focusedBorder`, `enabledBorder`, `inputTextStyle`, `hintStyle`, `hintText`, `suffixIcon`, `prefixIcon`, `backgroundColor`, `controller`, and `keyboardType`.
    
    Additionally, added a `CustomSearch` widget in `lib/features/home/presentation/view/widget/custom_search.dart` that utilizes the `SearchTextField` to create a styled search input with custom icons for filtering and

---
+++ lib/features/home/presentation/view/widget/custom_search.dart
@@ -0,0 +1,38 @@
+import 'package:e_commerce/core/constants/images.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../../core/widget/search_text_field.dart';
+
+class CustomSearch extends StatelessWidget {
+  const CustomSearch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Color(0xff0A000000),
+          blurRadius: 9,
+          offset: Offset(0, 2),
+          spreadRadius: 0,
+        ),
+      ]),
+      child: SearchTextField(
+        hintText: 'ابحث عن.......',
+        suffixIcon: SizedBox(
+            width: 20.w,
+            child: Center(
+                child: SvgPicture.asset(Assets.assetsImagesFilter))),
+        prefixIcon: SizedBox(
+            width: 24.w,
+            child: Center(
+                child:
+                    SvgPicture.asset(Assets.assetsImagesSearchIcon))),
+      ),
+    );
+  }
+}
