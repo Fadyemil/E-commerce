9f1fe956c27d8c9bee5d9247ff5bd15550c9c75b
fady emil <fady46t45f@gmail.com>
2024-11-14 21:43:15 +0200

    Refactor DeshBoardView and DeshBoardViewBody Widgets

--- lib/desh_board/features/desh_board/presentation/view/desh_board_view.dart
+++ lib/desh_board/features/desh_board/presentation/view/desh_board_view.dart
@@ -7,7 +7,7 @@ class DeshBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DeshBoardViewBody(),
+      body:  DeshBoardViewBody(),
     );
   }
 }
--- lib/desh_board/features/desh_board/presentation/view/widget/desh_board_view_body.dart
+++ lib/desh_board/features/desh_board/presentation/view/widget/desh_board_view_body.dart
@@ -1,13 +1,32 @@
+import 'package:e_commerce/core/theme/app_text_styles.dart';
+import 'package:e_commerce/core/widget/cusstom_button.dart';
+import 'package:e_commerce/desh_board/core/helper_functions/router/router_name_d_b.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DeshBoardViewBody extends StatelessWidget {
   const DeshBoardViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text('pfksdk'),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CusstomButton(
+            buttonText: 'add Data',
+            textStyle: AppTextStyles.bodyLargeRegular19,
+            onPressed: () {
+              context.pushNamed(RouterNameDB.addProduct);
+            },
+          ),
+          CusstomButton(
+            buttonText: 'update Data',
+            textStyle: AppTextStyles.bodyLargeRegular19,
+            onPressed: () {},
+          )
+        ],
       ),
     );
   }
