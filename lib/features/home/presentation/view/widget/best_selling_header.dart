0817a7d86766e7b66bb25d98efeb25e9e618698a
fady emil <fady46t45f@gmail.com>
2024-11-14 18:03:44 +0200

    Add NotificationWidget and Update BestSellingHeader
    Created a new `NotificationWidget` in the `best_selling_fruits` feature to display a notification icon with a custom shape and color. This widget uses `flutter_svg` to render the SVG asset and is styled with a green background.
    
    Updated the `BestSellingHeader` widget in the `home` feature to include an `InkWell` wrapper that navigates to the best-selling products page when tapped. The header now includes a "المزيد" (more) link that triggers the navigation using `GoRouter`.

--- lib/features/home/presentation/view/widget/best_selling_header.dart
+++ lib/features/home/presentation/view/widget/best_selling_header.dart
@@ -1,28 +1,35 @@
+import 'package:e_commerce/core/helper_functions/router/router_name.dart';
 import 'package:e_commerce/core/theme/app_colors.dart';
 import 'package:e_commerce/core/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BestSellingHeader extends StatelessWidget {
   const BestSellingHeader({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Text(
-            'الأكثر مبيعًا',
-            style: AppTextStyles.bodyBasaBold16.copyWith(color: Colors.black),
-          ),
-          Spacer(),
-          Text(
-            'المزيد',
-            style: AppTextStyles.bodySmallBold13.copyWith(
-              color: AppColors.grayscale400,
+    return InkWell(
+      onTap: () {
+        context.pushNamed(RouterName.bestSelling);
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Text(
+              'الأكثر مبيعًا',
+              style: AppTextStyles.bodyBasaBold16.copyWith(color: Colors.black),
             ),
-          ),
-        ],
+            Spacer(),
+            Text(
+              'المزيد',
+              style: AppTextStyles.bodySmallBold13.copyWith(
+                color: AppColors.grayscale400,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
