a3a1ff6528d5ce23e9e4081aca957aa10b26a6fe
fady emil <fady46t45f@gmail.com>
2024-11-14 21:21:19 +0200

    Add DeshBoardView and DeshBoardViewBody Widgets

--- lib/desh_board/features/desh_board/presentation/view/desh_board_view.dart
+++ lib/desh_board/features/desh_board/presentation/view/desh_board_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/desh_board/features/desh_board/presentation/view/widget/desh_board_view_body.dart';
 import 'package:flutter/material.dart';
 
 class DeshBoardView extends StatelessWidget {
@@ -6,9 +7,7 @@ class DeshBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('DeshBoardView'),
-      ),
+      body: DeshBoardViewBody(),
     );
   }
 }
---
+++ lib/desh_board/features/desh_board/presentation/view/widget/desh_board_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DeshBoardViewBody extends StatelessWidget {
+  const DeshBoardViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('pfksdk'),
+      ),
+    );
+  }
+}
